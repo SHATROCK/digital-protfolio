4203cbef4462976d46748427bdea1c68761d60e9
SHATROCK <shatrock5@gmail.com>
2025-03-20 13:48:36 +0530

    Delete pro.js and css and js file are becomes internal style

--- pro.js
+++
@@ -1,9 +0,0 @@
-let min=50;
-let max=100;
-const btn=document.getElementById("btn");
-const lable=document.getElementById("lable");
-let randomnum;
-btn.onclick=function(){
-    randomnum=Math.floor(Math.random()*(min - max))+min;
-    lable.innerHTML="your lucky number is:"+randomnum;
-}
